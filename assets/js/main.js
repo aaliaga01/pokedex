3c4285fc26409afa75387b44ea6f9c26804e53fc
aaliaga <a.aliaga.val@gmail.com>
2017-07-07 10:14:04 -0400

    peleando con json

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-alert("holas");
+$(document).ready function($){
+
+	$.ajax({
+		url: 'http://pokeapi.co/api/v2/pokedex/1/',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {limit: '811'},
+	})
+	.done(function(respt) {
+		console.log("respt");
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+	
+	
+};

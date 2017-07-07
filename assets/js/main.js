faa3def696b5f8ae7ed26b7e7b46173642e35c70
aaliaga <a.aliaga.val@gmail.com>
2017-07-07 13:34:37 -0400

    funciono pero me ayudaron

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {
+$(document).ready (function(){
+
 	$.ajax({
-		url: "http://pokeapi.co/api/v2/pokedex/1/",
-		type: "GET",
-		datatype: "json",
-		data: {limit:"811"},
-		success: function(source){
-			data = source;
-			showInfo();
-			},
-		error: function(dato){
-			alert("ERROR");
-			}
+
+		url: 'http://pokeapi.co/api/v2/pokemon',
+		type: 'GET',
+		dataType: 'JSON',
+		data: {'limit': '6'},
+	})
+	.done(function(r) {
+		mostrar(r.results);
+		console.log(r);
+		console.log("success");
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
 	});
+
+	var mostrar = function(data){
+
+        data.forEach(function(e){
+            var nombre = e.name;
+            $("#caja").append("<img class='pokeImg' src='http://img.pokemondb.net/artwork/"
+            	+ nombre + ".jpg'><p>"+ nombre +"</p></div>");
+        });
+    };	
 
 });
 
-function showInfo(){
-	$("#caja").append(data["data1"]);
 
-	$.each(data["data2"], function(index, value) {
-		$("#caja").append('index: ' + index + ' value1: ' + data['data2'][index]['value1'] + 
-			'value2: ' + data['data2'][index]['value2']);
-	});
-}
+	

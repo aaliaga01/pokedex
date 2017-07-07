8c303465868d6f3c467d0d8f1b268543dbddea8c
aaliaga <a.aliaga.val@gmail.com>
2017-07-07 12:06:43 -0400

    no funciona

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,25 @@
-$(document).ready (function($){
-		
-	var dibujar= function(data){
-		
-		var nombre="";
-		data.forEach(function (element){
-			nombre= element;
-			$("#caja").append(armar(nombre));
-		});
-	}
-
-	var armar = function(nombre){
-		var poke= "<p>" + gif + "</p>"
-		return poke;
-	}
-
-	var pokedex = function(pok){
-		$.ajax({
-			url: "http://pokeapi.co/api/v2/pokedex/1/",
-			type: "GET",
-			datatype: "json",
-			data: {limit:"811"}
-		})
-		.done(function(response){
-			console.log(response);
-			dibujarGifs(response.data);
-		})
-		.fail(function(){
-			console.log("error");
-		});
-	}
-
-	$("#boton").click(function(event){
-		console.log("Entro");
-		$("#caja").empty();
-		var poke=$("#poke").val();
-		dibujar(name);
-
+$(document).ready(function() {
+	$.ajax({
+		url: "http://pokeapi.co/api/v2/pokedex/1/",
+		type: "GET",
+		datatype: "json",
+		data: {limit:"811"},
+		success: function(source){
+			data = source;
+			showInfo();
+			},
+		error: function(dato){
+			alert("ERROR");
+			}
+	});
 
 });
 
+function showInfo(){
+	$("#caja").append(data["data1"]);
+
+	$.each(data["data2"], function(index, value) {
+		$("#caja").append('index: ' + index + ' value1: ' + data['data2'][index]['value1'] + 
+			'value2: ' + data['data2'][index]['value2']);
 	});
+}

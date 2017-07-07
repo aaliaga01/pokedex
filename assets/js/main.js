5c0ee8cddfaa042809584a76ddea78c44088db1e
aaliaga <a.aliaga.val@gmail.com>
2017-07-07 11:55:03 -0400

    no se que hago

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,42 @@
 $(document).ready (function($){
+		
+	var dibujar= function(data){
+		
+		var nombre="";
+		data.forEach(function (element){
+			nombre= element;
+			$("#caja").append(armar(nombre));
+		});
+	}
+
+	var armar = function(nombre){
+		var poke= "<p>" + gif + "</p>"
+		return poke;
+	}
+
+	var pokedex = function(pok){
 		$.ajax({
-		url: 'http://pokeapi.co/api/v2/pokedex/1/',
-		type: 'GET',
-		dataType: 'JSON',
-		data: {q:pokemon_entries},
-	})
-	.done(function(respuesta) {
-		console.log("respuesta");
-		console.log("success");
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
+			url: "http://pokeapi.co/api/v2/pokedex/1/",
+			type: "GET",
+			datatype: "json",
+			data: {limit:"811"}
+		})
+		.done(function(response){
+			console.log(response);
+			dibujarGifs(response.data);
+		})
+		.fail(function(){
+			console.log("error");
+		});
+	}
+
+	$("#boton").click(function(event){
+		console.log("Entro");
+		$("#caja").empty();
+		var poke=$("#poke").val();
+		dibujar(name);
 
 
 });
+
+	});

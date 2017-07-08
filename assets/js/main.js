5dfcc417c348957b351c56fcf58febc4ee501529
aaliaga-casa <a.aliaga.val@gmail.com>
2017-07-07 22:09:23 -0300

    despues hoooras se ven pokemones ordenados TOT

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready (function(){
 
 	$.ajax({
-
 		url: 'http://pokeapi.co/api/v2/pokemon',
 		type: 'GET',
 		dataType: 'JSON',
@@ -22,11 +21,21 @@ $(document).ready (function(){
 	var mostrar = function(data){
 
         data.forEach(function(e){
-            var nombre = e.name;
-            $("#lista-pokemones").append("<li><img class='pokeImg' src='http://img.pokemondb.net/artwork/"
-            	+ nombre + ".jpg'><p>"+ nombre +"</p><li>");
+            var nombre = e.name; //cada elemento sobre el cual itera, (el nombre) se guarda en una variable
 
-            $("#caja").addClass("")
+            var div = $("<div>", //Se crea un div
+				{
+				id: "poke", //atributo directo, nombre del id
+					"class": "col-sm-12 col-md-4 col-lg-3", //class entre comillas porque es una palabra reservada en javascript,
+					//le agrege la clase de bootstrap				
+				});
+
+            $(".row").append(div);//se inserta el div creado dinamicamente en el html
+
+            $(div).append("<img class='imagen' src='http://img.pokemondb.net/artwork/"
+            	+ nombre + ".jpg'>"); //se coloca la imagen dentro de cada col de bootstrap en el div creado
+
+            $(div).append("<h3>" + nombre + "</h3>")// para poner el nombre de cada pokemon
         });
     };	
 
